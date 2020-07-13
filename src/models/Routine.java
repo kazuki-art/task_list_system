41467609606732840d87f704ab94c2e088a09a86
kazuki.sato <toaru1018@yahoo.co.jp>
2020-07-13 14:57:50 +0900

    Add Routine class

---
+++ src/models/Routine.java
@@ -0,0 +1,90 @@
+package models;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "routines")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllRoutines",
+            query = "SELECT r FROM Routine AS r ORDER BY r.id DESC"
+            ),
+    @NamedQuery(
+            name = "getRoutinesCount",
+            query = "SELECT COUNT(r) FROM Routine AS r"
+            ),
+})
+
+@Entity
+public class Routine {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "routine_date", nullable = false)
+    private Date routine_date;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getRoutine_date() {
+        return routine_date;
+    }
+
+    public void setRoutine_date(Date routine_date) {
+        this.routine_date = routine_date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
